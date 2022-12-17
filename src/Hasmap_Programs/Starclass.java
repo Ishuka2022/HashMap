e90d0ca23cce9d81fa62afb92f104549b3249070
IshukaGods <igodshelwar14@gmail.com>
2022-12-17 15:23:42 +0530

    changed done

--- src/Hasmap_Programs/Starclass.java
+++ src/Hasmap_Programs/Starclass.java
@@ -2,10 +2,15 @@ package Hasmap_Programs;
 
 public class Starclass {
 	public static void main(String[] args) {
-		for(int i=0;i<=10;i++)
+		for(int i=0;i<=2;i++)
 		{
-			System.out.println("*  *");
-			System.out.println("Im very happy to know new things");
+			for(int j=0;j<=i;j++)
+			{
+			System.out.println("*     * *");
+			}
 		}
-	}
+			System.out.println("Im very happy to know new things");
+			System.out.println("----"+(" ")+"----");
+			System.out.println("Tu mera hai bas mera hii rahe ga");
+		}
 }
